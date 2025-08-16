69c4d59e9ae71edfecab98a2debebc38b3f4bd5c
kalid-2024 <kalidmohammed236@gmail.com>
2025-08-17 01:01:07 +0300

    added the phrase except  mysql.connector.Error

--- MySQLServer.py
+++ MySQLServer.py
@@ -13,5 +13,14 @@ def create_database():
             cursor = connection.cursor()
             cursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
             print('Database "alx_book_store" is Created Successfully!!!') 
-    execpt Error as e:
+    except mysql.connector.Error as e:
         print(f'Error while connecting to MYSQL: {e}')
+
+    finally:
+        if 'cursor' in locals() and cursor is not None:
+            cursor.close()
+        if 'connection' in locals() and connection.is_connected()
+            connection.close()
+
+if __name__ == __main__ 
+    create_database()
