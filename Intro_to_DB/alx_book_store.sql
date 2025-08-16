9ad5cd8fc016eb7c8396d26f21da60d4c61406e3
kalid-2024 <kalidmohammed236@gmail.com>
2025-08-16 22:33:27 +0300

    feat(added ' IF NOT EXISTS'): STATMENT IN THE CREATE DATABASE STATMENT

---
+++ Intro_to_DB/alx_book_store.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
+
+CREATE TABLE Author (
+    author_id Primary Key,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Books (
+    book_id Primary Key,
+    title VARCHAR(130),
+    author_id Foreign Key referencing Authors table,
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Customers (
+    customer_id Primary Key,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id Primary Key,
+    customer_id Foreign Key referencing Customers table,
+    order_date DATE
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid Primary Key,
+    order_id Foreign Key referencing Orders table,
+    book_id Foreign Key referencing Books table,
+    quantity DOUBLE
+);
+
